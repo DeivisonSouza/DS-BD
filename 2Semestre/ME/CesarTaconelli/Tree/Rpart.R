d4988e335f72a943023d697f00e8174a326903b4
Deivison <souzaflorestal@gmail.com>
2018-11-18 19:54:00 -0200

    update tree model

--- 2Semestre/ME/CesarTaconelli/Tree/Rpart.R
+++ 2Semestre/ME/CesarTaconelli/Tree/Rpart.R
@@ -63,9 +63,12 @@ rpart.plot(tree.cp1)
 set.seed(1)
 (tree.cp2 <- rpart(Rings ~ ., data = train, method="anova", control = rpart.control(cp = 0.01)))
 print(tree.cp2)
-summary(tree.cp2, cp = 0.02)      # um resumo da árvore (imprimindo divisões com cp superiores à 0.02)
+summary(tree.cp2, cp = 0.02)                    # um resumo da árvore (imprimindo divisões com cp superiores à 0.02)
 rpart.plot(tree.cp2)
-cp <- printcp(tree.cp2)           # Tabela de custo-complexidade.
+cp <- printcp(tree.cp2)                         # Tabela de custo-complexidade.
+
+unname(tail(tree.cp2$cptable[, "nsplit"], 1))   # número de splits (divisoes do modelo)
+
 
 barplot(tree.cp2$variable.importance, col = 'red', las = 1, 
         xlab = 'Covariável', ylab = 'Importância')
@@ -80,7 +83,7 @@ abline(v = 7, lty = "dashed", col = "red")
 
 cp <- printcp(tree.cp2)
 #------------------------------------------------------------------------
-# Agora, fazendo a poda usando a regra de Breiman
+# Agora, fazendo a poda usando a regra de Breiman...
 
 # Para essa configuração uma árvore com 7 divisões estaria dentro da regra de Breiman? Ou seja, eu deveria usar cp = 0.013786 para podar
 # a arvore (teria uma árvore com 7 divisões e 8 nós terminais)
@@ -134,13 +137,19 @@ abline(h=0, lty = "dashed", col = "red")
 
 
 #------------------------------------------------------------------------
-# Fazendo as previsões para o conjunto de teste
+# Soma de quadrados de resíduos
+library(Metrics)
+sum((test$Rings-test$pred)^2)     # manual
+sum(se(test$Rings, test$pred))    # usando pacote "metrics"
 
-(test$pred <- predict(prune.cp2, newdata = test))
+# Mean Squared Error
+mse(test$Rings, test$pred)
+mean((test$Rings - test$pred)^2)
 
-(SSE <- sum((test$Rings - mean(test$Rings))^2))
-(MSE <- sum((test$Rings - mean(test$Rings))^2)/length(test$Rings))
-(RMSE <- sqrt(MSE))
+# Root Mean Squared Error
+
+rmse(test$Rings, test$pred)
+sqrt(mean((test$Rings - test$pred)^2))
 
 plot(test$pred,test$Rings,xlab="Predito", ylab="Observado")
 abline(a=0, b=1)
@@ -152,33 +161,103 @@ abline(a=0, b=1)
 
 set.seed(1)
 (tree.cp3 <- rpart(Rings ~ ., data = train, method="anova", control = rpart.control(cp = 0.001)))
-
 print(tree.cp3)
-summary(tree.cp3, cp = 0.02)      # um resumo da árvore (imprimindo divisões com cp superiores à 0.02)
+summary(tree.cp3, cp = 0.02)                    # um resumo da árvore (imprimindo divisões com cp superiores à 0.02)
 rpart.plot(tree.cp3)
-cp <- printcp(tree.cp3)         # Tabela de custo-complexidade.
+cp <- printcp(tree.cp3)                         # Tabela de custo-complexidade.
+unname(tail(tree.cp3$cptable[, "nsplit"], 1))   # número de divisões
 
-par(mfrow=c(2,1), mar=c(4,5,5,3)) # mar(bottom, left, top, right)
+barplot(tree.cp3$variable.importance, col = 'red', las = 1, xlab = 'Covariável', ylab = 'Importância')
 
-
-# Fazendo os plots a árvore original teria 69 nós terminais e 68 divisões (árvore super ajustada)
-
+#-------------------------------------------------------------------------
+# Fazendo os plots a árvore original teria 68 divisões, e 69 nós terminais
 plotcp(tree.cp3, upper = "splits",  minline = TRUE)     # Usando "splits" fica mais fácil ver onde cortar a árvore...          
+abline(v = 15, lty = "dashed", col = "red")             # visulamente, o ponto de corte seria em 15 divisões (cp = 0.0056785)
+
+plotcp(tree.cp3, upper = "size",  minline = TRUE)       # o que correposnde também a 16 nós terminais
 abline(v = 15, lty = "dashed", col = "red")
 
-plotcp(tree.cp3, upper = "size",  minline = TRUE)
-abline(v = 15, lty = "dashed", col = "red")
+#------------------------------------------------------------------------
+# Agora, fazendo a poda usando a regra de Breiman.
+
+# Usando a regra 1-SE a uma árvore com 15 divisões fará o mesmo trabalho que a árvore de 68 divisões, 
+# com a vantagem de ser mais pacimoniosa.
+# Observar que depois da poda permaneceram apenas as variáveis: ShellWeighs e ShuckedWeight
+
+(prune.cp3 = prune(tree.cp3, cp = cp[15,1]))     
+plotcp(prune.cp3, upper = "splits",  minline = TRUE)
+plot(prune.cp3, branch = 0.3, compress = TRUE)
+text(prune.cp3)
+rpart.plot(prune.cp3, type = 3, cex = 1.2, uniform = FALSE)
+printcp(prune.cp3)
+
+#------------------------------------------------------------------------
+# Residuos para o modelo ajustado
+plot(residuals(prune.cp3) ~ predict(prune.cp3), col = 'blue', xlab = 'Valores ajustados', ylab = 'Resíduos')
+abline(h=0, lty = "dashed", col = "red")
+
+#------------------------------------------------------------------------
+# Predição usando a árvore final podada (cp = 0.001)
+
+#(new_data <- data.frame(ShellWeight = c(0.18, 0.31), 
+#                        ShuckedWeight = c(0.25, 0.45)))
 
 
-# Agora, fazeno a poda usando a regra de Breiman:
-# Para essa configuração uma árvore com 16 divisões estaria dentro da regra de Breiman? Ou seja, eu deveria usar cp = 0.0056785
-# para podar a arvore (teria uma árvore com 16 divisões e 17 nós terminais)
-(prune.cp3 = prune(tree.cp3, cp = 0.0056785))     
+(new_data <- data.frame(Type = as.factor(c(NA,NA)),
+                        LongestShell = as.numeric(c(NA,NA)),
+                        Diameter = as.numeric(c(NA,NA)),
+                        Height = as.numeric(c(NA,NA)),
+                        WholeWeight = as.numeric(c(NA,NA)),
+                        ShuckedWeight = c(0.25, 0.45),
+                        VisceraWeight = as.numeric(c(NA,NA)),
+                        ShellWeight = c(0.18, 0.31),
+                        stringsAsFactors = TRUE))
 
-# Mas, e usando o valor de cp = 0.001?
+(pred <- predict(prune.cp3, newdata = new_data))
 
-(prune.cp3 = prune(tree.cp3, cp = 0.001)) # a poderia seria realizada resultando em uma árvore mais complexa qdo comparada ao ponto
-# de poda sugerido pela regra de Breiman.
+#------------------------------------------------------------------------
+# Resíduo para os novos dados, sabendo que os valores reais para Rings são: 
+# Rings=8 e Rings=10, para o primeiro e segundo, respectivamente.
+
+(df <- cbind(new_data[,c(6,8)], observado = c(8,10), predito = pred))
+
+(residuo <- df$predito - df$observado)
+
+#------------------------------------------------------------------------
+# Fazendo as previsões para o conjunto de teste
+#------------------------------------------------------------------------
+# Soma de quadrados de resíduos
+
+test$pred <- predict(prune.cp3, newdata = test)
+
+library(Metrics)
+sum((test$Rings-test$pred)^2)     # manual
+sum(se(test$Rings, test$pred))    # usando pacote "metrics"
+
+# Mean Squared Error
+mse(test$Rings, test$pred)
+mean((test$Rings - test$pred)^2)
+
+# Root Mean Squared Error
+
+rmse(test$Rings, test$pred)
+sqrt(mean((test$Rings - test$pred)^2))
+
+
+#Árvore customizada
+heat.tree <- function(tree, low.is.green = FALSE, ...) { # dots args passed to prp
+  y <- tree$frame$yval
+  if(low.is.green)
+    y <- -y
+  max <- max(y)
+  min <- min(y)
+  cols <- rainbow(99, end = .36)[
+    ifelse(y > y[1], (y-y[1]) * (99-50) / (max-y[1]) + 50,
+           (y-min) * (50-1) / (y[1]-min) + 1)]
+  prp(tree, branch.col = cols, box.col = cols, ...)
+}
+
+heat.tree(prune.cp3, type = 4, varlen = 0, faclen = 0, fallen.leaves = TRUE)
 
 #----------------------------------------------------------------------------------------------------------------------------------
 # Usando a biblioteca "tree"
